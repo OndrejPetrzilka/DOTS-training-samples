43559e59aa0910fead6c85e4bec389291b6c0c80
Ondrej Petrzilka <ondrej.petrzilka@gmail.com>
2020-08-01 09:40:54 +0200

    * AntRendering, using instaned draw, but it's blinking

--- Ported/AntPhermonesEcs/Assets/Scripts/Presentation/AntRendering.cs
+++ Ported/AntPhermonesEcs/Assets/Scripts/Presentation/AntRendering.cs
@@ -4,17 +4,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Unity.Collections;
+using Unity.Collections.LowLevel.Unsafe;
 using Unity.Entities;
 using Unity.Mathematics;
 using UnityEngine;
 
 [UpdateInGroup(typeof(PresentationSystemGroup))]
-public class AntRendering : ComponentSystem
+public class AntRendering : SystemBase
 {
     AntSettings m_settings;
     AntSettingsData m_settingsData;
     NativeArray<Matrix4x4> m_rotationMatrices;
     MaterialPropertyBlock m_block;
+
+    Matrix4x4[] m_matrices = new Matrix4x4[1023];
+    Vector4[] m_colors = new Vector4[1023];
 
     protected override void OnCreate()
     {
@@ -40,21 +44,37 @@ public class AntRendering : ComponentSystem
         base.OnDestroy();
     }
 
-    protected override void OnUpdate()
+    protected unsafe override void OnUpdate()
     {
-        Entities.WithAllReadOnly<AntTag, Position, FacingAngle, ColorData>().ForEach((Entity e, ref Position position, ref FacingAngle facingAngle, ref ColorData color) =>
+        int count = 0;
+        var rotations = m_rotationMatrices;
+        var settings = m_settingsData;
+
+        fixed (Matrix4x4* matrixPtr = m_matrices)
+        fixed (Vector4* colorPtr = m_colors)
         {
-            float angle = facingAngle.Value;
-            angle /= Mathf.PI * 2f;
-            angle -= Mathf.Floor(angle);
-            angle *= m_settingsData.rotationResolution;
+            var matrices = matrixPtr;
+            var colors = colorPtr;
+            var countPtr = &count;
 
-            Matrix4x4 matrix = m_rotationMatrices[((int)angle) % m_settingsData.rotationResolution];
-            matrix.m03 = position.Value.x / m_settingsData.mapSize;
-            matrix.m13 = position.Value.y / m_settingsData.mapSize;
+            Entities.WithNativeDisableUnsafePtrRestriction(countPtr).WithNativeDisableUnsafePtrRestriction(matrices).WithNativeDisableUnsafePtrRestriction(colors).WithAll<AntTag>().ForEach((Entity e, in Position position, in FacingAngle facingAngle, in ColorData color) =>
+            {
+                float angle = facingAngle.Value;
+                angle /= Mathf.PI * 2f;
+                angle -= Mathf.Floor(angle);
+                angle *= settings.rotationResolution;
 
-            m_block.SetVector("_Color", color.Value);
-            Graphics.DrawMesh(m_settings.antMesh, matrix, m_settings.antMaterial, 0, null, 0, m_block);
-        });
+                Matrix4x4 matrix = rotations[((int)angle) % settings.rotationResolution];
+                matrix.m03 = position.Value.x / settings.mapSize;
+                matrix.m13 = position.Value.y / settings.mapSize;
+
+                matrices[*countPtr] = matrix;
+                colors[*countPtr] = color.Value;
+                (*countPtr)++;
+            }).ScheduleParallel(Dependency).Complete();
+        }
+
+        m_block.SetVectorArray("_Color", m_colors);
+        Graphics.DrawMeshInstanced(m_settings.antMesh, 0, m_settings.antMaterial, m_matrices, count, m_block);
     }
 }
