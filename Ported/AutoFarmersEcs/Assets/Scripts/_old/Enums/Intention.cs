71bcc8b1c67b9cef8a67adc13782810f32a06970
Ondrej Petrzilka <ondrej.petrzilka@gmail.com>
2020-08-02 13:03:35 +0200

    + AutoFarmersEcs (copied old project)

---
+++ Ported/AutoFarmersEcs/Assets/Scripts/_old/Enums/Intention.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Intention
+{
+    None,
+    SmashRocks,
+    TillGround,
+    PlantSeeds,
+    SellPlants
+}
