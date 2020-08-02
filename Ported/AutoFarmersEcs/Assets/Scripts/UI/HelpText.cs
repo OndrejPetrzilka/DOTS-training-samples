daaaaf0bbdc0c63e494469c296f1a6d0580abe5f
Ondrej Petrzilka <ondrej.petrzilka@gmail.com>
2020-08-02 17:32:23 +0200

    + help text, free camera, time control

---
+++ Ported/AutoFarmersEcs/Assets/Scripts/UI/HelpText.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HelpText : MonoBehaviour
+{
+    public Text Text;
+
+    float timeScale;
+
+    private void Update()
+    {
+        if (Time.timeScale != timeScale)
+        {
+            timeScale = Time.timeScale;
+            Text.text = $"Time scale: {timeScale}";
+        }
+    }
+}
--- Ported/AutoFarmersEcs/Assets/Scripts/_old/CamFollow.cs
+++ Ported/AutoFarmersEcs/Assets/Scripts/_old/CamFollow.cs
@@ -1,24 +1,89 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using Unity.Entities;
 using UnityEngine;
 
-public class CamFollow : MonoBehaviour {
+public class CamFollow : MonoBehaviour
+{
+    public Vector2 viewAngles;
+    public float viewDist;
+    public float mouseSensitivity;
 
-	public Vector2 viewAngles;
-	public float viewDist;
-	public float mouseSensitivity;
+    public bool freeCamera = false;
+    public float moveSpeed = 1;
+    public float zoomSpeed = 1;
 
-	void Start () {
-		transform.rotation = Quaternion.Euler(viewAngles.y,viewAngles.x,0f);
-	}
-	
-	void LateUpdate () {
-		Vector3 pos = FarmerManager.instance.firstFarmer.GetSmoothWorldPos();
-		viewAngles.x += Input.GetAxis("Mouse X") * mouseSensitivity/Screen.height;
-		viewAngles.y -= Input.GetAxis("Mouse Y") * mouseSensitivity/Screen.height;
-		viewAngles.y = Mathf.Clamp(viewAngles.y,7f,80f);
-		viewAngles.x -= Mathf.Floor(viewAngles.x / 360f) * 360f;
-		transform.rotation = Quaternion.Euler(viewAngles.y,viewAngles.x,0f);
-		transform.position = pos - transform.forward * viewDist;
-	}
+    void Start()
+    {
+        viewAngles.y = transform.localEulerAngles.x;
+        viewAngles.x = transform.localEulerAngles.y;
+    }
+
+    void LateUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            freeCamera = !freeCamera;
+        }
+
+        if (freeCamera)
+        {
+            if (Input.GetKey(KeyCode.W))
+            {
+                Move(Vector3.forward);
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                Move(Vector3.back);
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                Move(Vector3.left);
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                Move(Vector3.right);
+            }
+            if (Input.mouseScrollDelta.y != 0)
+            {
+                var pos = transform.position;
+                pos.y = Mathf.Clamp(pos.y * Mathf.Pow(1 + zoomSpeed, -Input.mouseScrollDelta.y), 1, 50);
+                transform.position = pos;
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                if (Input.GetKeyDown(KeyCode.Alpha0 + i))
+                {
+                    int value = i == 0 ? 10 : i;
+                    if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
+                    {
+                        value = value * value;
+                    }
+                    Time.timeScale = value;
+                }
+            }
+        }
+        else
+        {
+            Vector3 pos = FarmerManager.instance.firstFarmer.GetSmoothWorldPos();
+            transform.position = pos - transform.forward * viewDist;
+        }
+
+        transform.rotation = Quaternion.Euler(viewAngles.y, viewAngles.x, 0f);
+        viewAngles.x += Input.GetAxis("Mouse X") * mouseSensitivity / Screen.height;
+        viewAngles.y -= Input.GetAxis("Mouse Y") * mouseSensitivity / Screen.height;
+        viewAngles.y = Mathf.Clamp(viewAngles.y, 7f, 80f);
+        viewAngles.x -= Mathf.Floor(viewAngles.x / 360f) * 360f;
+    }
+
+    private void Move(Vector3 localDirection)
+    {
+        // Move only in flat plane
+        var worldDirection = transform.TransformDirection(localDirection);
+        worldDirection.y = 0;
+        worldDirection = worldDirection.normalized;
+        transform.position += worldDirection * moveSpeed * Time.time;
+    }
 }
