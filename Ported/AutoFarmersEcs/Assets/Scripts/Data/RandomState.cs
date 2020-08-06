59e5480172da6f0b7da62910ce4ef69632ab6b26
Ondrej Petrzilka <ondrej.petrzilka@gmail.com>
2020-08-06 22:27:09 +0200

    + FarmerDecision optimization

---
+++ Ported/AutoFarmersEcs/Assets/Scripts/Data/RandomState.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity.Entities;
+using Unity.Mathematics;
+using Random = Unity.Mathematics.Random;
+
+public struct RandomState : IComponentData
+{
+    public Random Rng;
+
+    public RandomState(uint seed)
+    {
+        Rng = new Random(seed);
+    }
+}
--- Ported/AutoFarmersEcs/Assets/Scripts/Simulation/FarmerDecision.cs
+++ Ported/AutoFarmersEcs/Assets/Scripts/Simulation/FarmerDecision.cs
@@ -4,39 +4,54 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Unity.Entities;
+using Unity.Mathematics;
 using UnityEngine;
-using Random = UnityEngine.Random;
+using Random = Unity.Mathematics.Random;
 
 [UpdateInGroup(typeof(FixedStepSimulationSystemGroup))]
 public class FarmerDecision : SystemBase
 {
+    EntityCommandBufferSystem m_cmdSystem;
+
     protected override void OnCreate()
     {
         base.OnCreate();
         RequireSingletonForUpdate<Settings>();
+        m_cmdSystem = World.GetOrCreateSystem<EndFixedStepSimulationEntityCommandBufferSystem>();
     }
 
     protected override void OnUpdate()
     {
-        Entities.WithStructuralChanges().WithAll<FarmerTag>().WithNone<WorkClearRocks, WorkPlantSeeds, WorkSellPlants>().WithNone<WorkTillGround>().ForEach((Entity e) =>
+        var cmdBuffer = m_cmdSystem.CreateCommandBuffer();
+
+        Entities.WithAll<FarmerTag>().WithNone<RandomState>().ForEach((Entity e, int entityInQueryIndex) =>
         {
-            int rand = Random.Range(0, 4);
+            cmdBuffer.AddComponent(e, new RandomState((uint)e.Index + 1));
+        }).Schedule();
+
+        cmdBuffer = m_cmdSystem.CreateCommandBuffer();
+
+        Entities.WithAll<FarmerTag>().WithNone<WorkClearRocks, WorkPlantSeeds, WorkSellPlants>().WithNone<WorkTillGround>().ForEach((Entity e, ref RandomState rng) =>
+        {
+            int rand = rng.Rng.NextInt(0, 4);
             if (rand == 0)
             {
-                EntityManager.AddComponent<WorkClearRocks>(e);
+                cmdBuffer.AddComponent<WorkClearRocks>(e);
             }
             else if (rand == 1)
             {
-                EntityManager.AddComponent<WorkTillGround>(e);
+                cmdBuffer.AddComponent<WorkTillGround>(e);
             }
             else if (rand == 2)
             {
-                EntityManager.AddComponent<WorkPlantSeeds>(e);
+                cmdBuffer.AddComponent<WorkPlantSeeds>(e);
             }
             else if (rand == 3)
             {
-                EntityManager.AddComponent<WorkSellPlants>(e);
+                cmdBuffer.AddComponent<WorkSellPlants>(e);
             }
-        }).Run();
+        }).Schedule();
+
+        m_cmdSystem.AddJobHandleForProducer(Dependency);
     }
 }
