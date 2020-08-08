51eded1290a3aa831674ce1efcd1055d94e96d83
Ondrej Petrzilka <ondrej.petrzilka@gmail.com>
2020-08-08 16:53:09 +0200

    + PlantGrowth now uses LookupComponentFilters

--- Ported/AutoFarmersEcs/Assets/Scripts/Simulation/PlantGrowth.cs
+++ Ported/AutoFarmersEcs/Assets/Scripts/Simulation/PlantGrowth.cs
@@ -10,19 +10,30 @@ using UnityEngine;
 [UpdateInGroup(typeof(FixedStepSimulationSystemGroup))]
 public class PlantGrowth : SystemBase
 {
+    EntityQuery m_query;
+
     protected override void OnCreate()
     {
         base.OnCreate();
         RequireSingletonForUpdate<Settings>();
+
+        m_query = GetEntityQuery(new EntityQueryDesc
+        {
+            All = new ComponentType[] { typeof(PlantTag) },
+            None = new ComponentType[] { typeof(LookupComponentFilters) },
+        });
     }
 
     protected override void OnUpdate()
     {
         float deltaTime = Time.fixedDeltaTime;
 
-        Entities.ForEach((Entity e, ref PlantTag plant) =>
+        EntityManager.AddComponent<LookupComponentFilters>(m_query);
+
+        Entities.ForEach((Entity e, ref PlantTag plant, ref LookupComponentFilters filters) =>
         {
             plant.Growth = Mathf.Min(plant.Growth + deltaTime / 10f, 1f);
+            filters.Value = plant.Growth == 1 ? (byte)1 : (byte)0;
         }).ScheduleParallel();
     }
 }
