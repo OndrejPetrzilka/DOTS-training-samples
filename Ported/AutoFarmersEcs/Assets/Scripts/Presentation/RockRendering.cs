8075fa7761443371c8d76dcae2d5a70c155a6d6a
Ondrej Petrzilka <ondrej.petrzilka@gmail.com>
2020-11-17 13:58:28 +0100

    * clearing rocks changes reduces their height during process

--- Ported/AutoFarmersEcs/Assets/Scripts/Presentation/RockRendering.cs
+++ Ported/AutoFarmersEcs/Assets/Scripts/Presentation/RockRendering.cs
@@ -23,11 +23,12 @@ public class RockRendering : SystemBase
         var mesh = m_settings.rockMesh;
         var material = m_settings.rockMaterial;
 
-        Entities.WithoutBurst().WithAll<RockTag>().ForEach((Entity entity, in Position position, in Size size, in Depth depth) =>
+        Entities.WithoutBurst().WithAll<RockTag>().ForEach((Entity entity, in Position position, in Size size, in Health health, in Depth depth) =>
         {
+            float maxHealth = (size.Value.x + 1) * (size.Value.y + 1) * 15;
             float2 center2D = position.Value + size.Value * 0.5f;
             float3 worldPos = new float3(center2D.x + .5f, depth.Value * .5f, center2D.y + .5f);
-            float3 scale = new float3(size.Value.x + .5f, depth.Value, size.Value.y + .5f);
+            float3 scale = new float3(size.Value.x + .5f, depth.Value * health.Value / maxHealth, size.Value.y + .5f);
             var matrix = Matrix4x4.TRS(worldPos, Quaternion.identity, scale);
             Graphics.DrawMesh(mesh, matrix, material, 0);
         }).Run();
